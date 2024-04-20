941f5afab24d57feb0b2fc4eaa26cb3ce2d172bd
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-04-20 23:00:43 +0700

    inventory-service: create + implement inventory service

---
+++ inventory-service/src/main/java/microservice/shop/inventoryservice/Repositories/InventoryRepository.java
@@ -0,0 +1,12 @@
+package microservice.shop.inventoryservice.Repositories;
+
+
+import microservice.shop.inventoryservice.Models.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
