c1010512f99a3abdc8b6d5e650f5f6864fc8504c
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-08-25 11:37:20 +0700

    chore: remove eureka server

--- inventory-service/src/main/java/microservice/shop/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/microservice/shop/inventoryservice/InventoryServiceApplication.java
@@ -2,10 +2,8 @@ package microservice.shop.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
