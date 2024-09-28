1ebc315fe3d654afaaf29d7b352f336b93c99de6
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-09-28 23:03:50 +0700

    feat: add springdoc for all service

---
+++ inventory-service/src/main/java/microservice/shop/inventoryservice/configs/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package microservice.shop.inventoryservice.configs;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Inventory Service API")
+                        .version("1.0")
+                        .description("Documentation Inventory Service API v1.0")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Inventory Service Wiki Documentation")
+                        .url("https://inventory-service-wiki.com"));
+    }
+}
---
+++ order-service/src/main/java/microservice/shop/orderservice/configs/OpenAPIConfig.java
@@ -0,0 +1,25 @@
+package microservice.shop.orderservice.configs;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order Service API")
+                        .version("1.0")
+                        .description("Documentation Order Service API v1.0")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Order Service Wiki Documentation")
+                        .url("https://order-service-wiki.com"));
+    }
+}
