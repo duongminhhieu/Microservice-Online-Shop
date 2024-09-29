f7b90238e3c38462ab301d19cc7edc9cd2077802
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-09-29 15:31:00 +0700

    refactor(order-service): using RestClient instead OpenFeign

--- order-service/src/main/java/microservice/shop/orderservice/clients/InventoryClient.java
+++ order-service/src/main/java/microservice/shop/orderservice/clients/InventoryClient.java
@@ -1,15 +1,13 @@
 package microservice.shop.orderservice.clients;
 
 import microservice.shop.orderservice.dtos.InventoryResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
 import java.util.List;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
-    @GetMapping(value = "/api/inventory")
+    @GetExchange(value = "/api/inventory")
     List<InventoryResponse> isInStock(@RequestParam("skuCode") List<String> skuCodes);
 }
